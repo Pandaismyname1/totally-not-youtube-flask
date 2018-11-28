b1f05fdbc54f20a860138e6e5f69763ee04a40a4
c322105 <Emanuel-Andrei.Paius@unicredit.eu>
2018-11-28 12:57:08 +0200

    queue test on server

--- front/player.js
+++ front/player.js
@@ -1,9 +1,31 @@
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// Global Vars
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+var queue = [];
+var queueIndex = 0;
+
+
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// Application
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+function init() {
+    document.getElementById('audioPlayer').addEventListener('ended', onSongEnded);
+}
+
+function queueEntry(link, title, author, thumbnail)
+{
+    this.link = link;
+    this.title = title;
+    this.author = author;
+    this.thumbnail = thumbnail;
+}
+
 function request(parameters,destination,callback)
 {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.open("GET", destination+objToUrlParams(parameters));
     xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200)
+        if (this.readyState === 4 && this.status === 200)
         {
             callback(this.response);
         }
@@ -11,25 +33,55 @@ function request(parameters,destination,callback)
     // xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     xmlhttp.send();
 }
+
 function objToUrlParams(obj)
 {
     var str = "";
     for (var key in obj) {
-        if (str != "") {
+        if (str !== "") {
             str += "&";
         }
         str += key + "=" + encodeURIComponent(obj[key]);
     }
     return str
 }
+
 function search() {
     var obj = {}
-    var query = document.getElementById('searchInput').value
-    obj['query'] = query
+    obj['query'] = document.getElementById('searchInput').value;
     request(obj,'/searchVideo?', searchCallback)
 }
+
 function searchCallback(url) {
+    let newSong = new queueEntry(url, null, null, null);
+    queue.push(newSong)
+}
+
+function playSong(url) {
     document.getElementById('audioPlayerSource').src = url;
     document.getElementById('audioPlayer').load();
     document.getElementById('audioPlayer').play();
+}
+
+function playNextSong() {
+    queueIndex += 1;
+    if(queueIndex>=queue.length)
+    {
+        queueIndex=0;
+    }
+    playSong(queue[queueIndex]);
+}
+
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// UI
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+function onPlayButtonPressed() {
+    playSong(queue[0]);
+}
+
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// Handlers
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+function onSongEnded() {
+    playNextSong();
 }
