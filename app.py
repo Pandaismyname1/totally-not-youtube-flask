0cc0926d6de3b6ca91bc76de8202f5240c4d2c9b
c322105 <Emanuel-Andrei.Paius@unicredit.eu>
2018-11-28 18:21:32 +0200

    fixed meta

--- app.py
+++ app.py
@@ -19,7 +19,9 @@ def resource(resource):
 def requestVideo():
     req = request.args['videoUrl']
     videos.downloadVideo(req)
+    print(req)
     meta = downloader.getMeta("https://www.youtube.com/watch?v=" +req)
+    print(meta)
     return 'cache?videoId='+utils.extractVideoID(req),meta[0],meta[1]
 
 
