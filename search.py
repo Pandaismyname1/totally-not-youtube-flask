2b6908a97d58e01107c52bcbaa5061699d73aa9b
c322105 <Emanuel-Andrei.Paius@unicredit.eu>
2018-11-29 13:00:40 +0200

    going crazy

--- search.py
+++ search.py
@@ -18,7 +18,14 @@ def searchOnYoutube(query):
     parameters.append(("key", getYoutubeApiKey()))
     requestUrl += utils.listToParams(parameters)
     print(requestUrl)
-    jsonText = urllib.request.urlopen(requestUrl).read()
+    hdr = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+        'Accept-Encoding': 'none',
+        'Accept-Language': 'en-US,en;q=0.8',
+        'Connection': 'keep-alive'}
+    jsonText = urllib.request.urlopen(urllib.request.Request(requestUrl, headers=hdr)).read()
     print(jsonText)
     jsonObj = json.loads(json)
     videos = extractDataFromJson(jsonObj)
