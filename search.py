2fb55b35d4a981dc43b18fcbc63ae4c274f4dc7e
c322105 <Emanuel-Andrei.Paius@unicredit.eu>
2018-11-29 11:13:15 +0200

    started working with youtube data api

--- search.py
+++ search.py
@@ -1,26 +1,40 @@
 import urllib.request
 import urllib.parse
 import datetime
+import utils
+import json
 import re
+
+
 def searchOnYoutube(query):
-    print('searchOnYoutube '+str(datetime.datetime.now()))
-    query_string = urllib.parse.urlencode({"search_query" : query})
-    html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
-    print('urllib '+str(datetime.datetime.now()))
-    # search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
-    search_results = searchInHTML(html_content.read().decode())
-    print('search '+str(datetime.datetime.now()))
-    return "https://www.youtube.com/watch?v=" + search_results[0]
+    print('searchOnYoutube ' + str(datetime.datetime.now()))
+    query_string = urllib.parse.urlencode({"search_query": query})
+    requestUrl = 'https://www.googleapis.com/youtube/v3/search?'
+    parameters = list()
+    parameters.append(("part", "snippet"))
+    parameters.append(("maxResults", "25"))
+    parameters.append(("type", "video"))
+    parameters.append(("q", query))
+    parameters.append(("key", getYoutubeApiKey()))
+    requestUrl += utils.listToParams(parameters)
+    jsonText = urllib.request.urlopen(requestUrl).read().decode()
+    jsonObj = json.loads(json)
+    videos = extractDataFromJson(jsonObj)
+    print('search ' + str(datetime.datetime.now()))
+    return "https://www.youtube.com/watch?v=" + videos[0]
+
+
 def searchInHTML(html):
     results = []
     query = 'href=\"/watch?v=xxxxxxxxxxx\"'
     currentResult = ''
     currentResultPosition = 0
     for chr in html:
-        if (len(query)-14 <= currentResultPosition and currentResultPosition < len(query)-1) or (currentResultPosition<len(query)-14 and chr == query[currentResultPosition]):
+        if (len(query) - 14 <= currentResultPosition and currentResultPosition < len(query) - 1) or (
+                currentResultPosition < len(query) - 14 and chr == query[currentResultPosition]):
             currentResult += chr
             currentResultPosition += 1
-            if currentResultPosition==len(query)-1:
+            if currentResultPosition == len(query) - 1:
                 results.append(currentResult[-11:])
                 currentResult = ''
                 currentResultPosition = 0
@@ -29,3 +43,16 @@ def searchInHTML(html):
             currentResultPosition = 0
     print(results)
     return results
+
+
+def extractDataFromJson(jsonObj):
+    results = []
+    for item in jsonObj['items']:
+        results.append((jsonObj['item']['id']['videoId'], jsonObj['item']['snippet']['title'],
+                        jsonObj['item']['snippet']['thumbnails']['medium']['url']))
+    return results
+
+
+def getYoutubeApiKey():
+    with open('../youtubeCredentials.txt', 'r') as f:
+        return f.readline()
--- utils.py
+++ utils.py
@@ -1,2 +1,11 @@
 def extractVideoID(url):
     return url.replace('https://www.youtube.com/watch?v=', '')
+
+
+def listToParams(li):
+    urlParams = ''
+    for index in range(len(li)):
+        if index != 0:
+            urlParams += '&'
+        urlParams += li[index][0]+'='+li[index][1]
+    return urlParams
